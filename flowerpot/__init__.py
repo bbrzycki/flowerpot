f41aaad1a0cb4e0c2820104c58c4b2566ad980fd
Bryan Brzycki <bbrzycki@berkeley.edu>
2018-06-24 21:35:43 -0700

    Add structure

---
+++ flowerpot/__init__.py
@@ -0,0 +1,11 @@
+"""
+flowerpot is an analysis and visualization package for blossom
+"""
+
+__version__ = "0.1"
+
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+
+from . import *
