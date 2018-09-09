4fb589d6ca6798acd6455750e1a8bf3bda1109e7
Bryan Brzycki <bbrzycki@college.harvard.edu>
2018-09-09 02:18:37 -0700

    Add visualization method to package, clean up visualization notebook and directory organization

--- flowerpot/__init__.py
+++ flowerpot/__init__.py
@@ -8,4 +8,4 @@ import sys
 import os
 sys.path.append(os.path.dirname(__file__))
 
-from . import *
+from visualization import *
---
+++ flowerpot/visualization.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import json
+import glob
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_organism_field(time, organism_paths, world_paths, field):
+    image = []
+    for i in range(time + 1):
+        with open(organism_paths[i], 'r') as f:
+            organism_list = json.load(f)
+        with open(world_paths[i], 'r') as f:
+            world = json.load(f)
+
+        matching_organisms = []
+        for organism_dict in organism_list:
+            if field == 'alive' and organism_dict['alive'] \
+            or field == 'old_age' and organism_dict['cause_of_death'] == 'old_age' \
+            or field == 'thirst' and organism_dict['cause_of_death'] == 'thirst' \
+            or field == 'hunger' and organism_dict['cause_of_death'] == 'hunger':
+                matching_organisms.append(organism_dict['position'])
+
+        frequency_weights = np.zeros(world['world_size'][0])
+        for position in matching_organisms:
+            index = position[0]
+            frequency_weights[index] += 1
+
+        image.append(frequency_weights)
+
+    plt.imshow(image, aspect='auto')
+    plt.colorbar()
+    plt.title('%s' % field)
+    # plt.show()
+
+def plot_world_field(time, world_paths, field):
+    image = []
+    for i in range(time + 1):
+        with open(world_paths[i], 'r') as f:
+            world = json.load(f)
+
+        image.append(world[field])
+
+    plt.imshow(image, aspect='auto')
+    plt.colorbar()
+    plt.title('%s' % field)
+    # plt.show()
