daffddcdaf666b0c508106137e8421a595b52399
Bryan Brzycki <bbrzycki@berkeley.edu>
2018-11-09 17:06:04 -0800

    Update visualization methods and make full snapshots in Jupyter notebook

--- flowerpot/visualization.py
+++ flowerpot/visualization.py
@@ -14,10 +14,14 @@ def plot_organism_field(time, organism_paths, world_paths, field):
         matching_organisms = []
         for organism_dict in organism_list:
             if field == 'alive' and organism_dict['alive'] \
-            or field == 'old_age' and organism_dict['cause_of_death'] == 'old_age' \
-            or field == 'thirst' and organism_dict['cause_of_death'] == 'thirst' \
-            or field == 'hunger' and organism_dict['cause_of_death'] == 'hunger' \
-            or field == 'births' and organism_dict['age'] == 0:
+                    or field == 'old_age' \
+                        and organism_dict['cause_of_death'] == 'old_age' \
+                    or field == 'thirst' \
+                        and organism_dict['cause_of_death'] == 'thirst' \
+                    or field == 'hunger' \
+                        and organism_dict['cause_of_death'] == 'hunger' \
+                    or field == 'births' \
+                        and organism_dict['age'] == 0:
                 matching_organisms.append(organism_dict['position'])
 
         frequency_weights = np.zeros(world['world_size'][0])
@@ -29,8 +33,21 @@ def plot_organism_field(time, organism_paths, world_paths, field):
 
     plt.imshow(image, aspect='auto')
     plt.colorbar()
-    plt.title('%s' % field)
-    # plt.show()
+
+    if field == 'alive':
+        title = 'Living Organisms'
+    elif field == 'old_age':
+        title = 'Deaths from Old Age'
+    elif field == 'thirst':
+        title = 'Deaths from Thirst'
+    elif field == 'hunger':
+        title = 'Deaths from Hunger'
+    elif field == 'births':
+        title = 'Organism Births'
+    else:
+        title = field
+    plt.title('%s' % title)
+
 
 def plot_world_field(time, world_paths, field):
     image = []
@@ -42,5 +59,13 @@ def plot_world_field(time, world_paths, field):
 
     plt.imshow(image, aspect='auto')
     plt.colorbar()
-    plt.title('%s' % field)
-    # plt.show()
+
+    if field == 'water':
+        title = 'Water Supply'
+    elif field == 'food':
+        title = 'Food Supply'
+    elif field == 'obstacles':
+        title = 'Obstacles'
+    else:
+        title = field
+    plt.title('%s' % title)
--- scripts/run_full_2d_sim.sh
+++
@@ -1,2 +0,0 @@
-python /Users/bryanbrzycki/Documents/Personal/Evolution-Code/Code/blossom/scripts/2d/parameter_load_2d.py
-python make_2d_images.py
